ec02731cef35a061b63947f2edc534a5527dd299
Albert <chenalbert60@gmail.com>
2023-01-25 19:11:08 +0000

    modified db connect

--- docker/db_connect.py
+++ docker/db_connect.py
@@ -2,32 +2,20 @@
 
 import psycopg2
 
-
 conn = psycopg2.connect( 
-
-    host="localhost", 
-
-    database="postgres", 
-
+    host="group2rds.ckokfd9swhyk.us-west-2.rds.amazonaws.com", 
+    database="group2rds", 
     user="postgres", 
+    password="password") 
+  
 
-    password="test") 
-
- 
- 
-
-cursor = conn.cursor() 
-
- 
- 
+cursor = conn.cursor()  
 
 #sannity check - am i actually connected to the database?? 
 
 cursor.execute('SELECT * FROM information_schema.tables') 
 
  
- 
-
 rows = cursor.fetchall() 
 
 for table in rows: 
