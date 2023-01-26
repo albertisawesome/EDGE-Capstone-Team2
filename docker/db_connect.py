4a83f0d94277c0fc10152e9473136c4a76065a06
Albert <chenalbert60@gmail.com>
2023-01-26 18:37:31 +0000

    asdfsadf

--- docker/db_connect.py
+++ docker/db_connect.py
@@ -13,15 +13,37 @@ cursor = conn.cursor()
 
 #sannity check - am i actually connected to the database?? 
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS ADDRESS(ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,ADDRESS VARCHAR(50) NOT NULL,CITY VARCHAR(25) NOT NULL,STATE CHAR(2) NOT NULL,ZIP_CODE CHAR(5) NOT NULL);''')
-print('Address table ready')
+cursor.execute(
+            """
+            CREATE TABLE Address (
+                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    address VARCHAR(50) NOT NULL,
+                    city VARCHAR(25) NOT NULL,
+                    state CHAR(2) NOT NULL,
+                    zip_code CHAR(5) NOT NULL);"""
+    )
 
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS CUSTOMER(ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,FIRST_NAME VARCHAR(25) NOT NULL,LAST_NAME VARCHAR(25) NOT NULL,ADDRESS_ID INTEGER NOT NULL,EMAIL_ADDRESS VARCHAR(50) NOT NULL,FOREIGN KEY (ADDRESS_ID)REFERENCES ADDRESS (ADDRESS_ID));''')
-print('Customer table ready')
+cursor.execute( 
+        """ CREATE TABLE IF NOT EXISTS Customer (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                first_name VARCHAR(25) NOT NULL,
+                last_name VARCHAR(25) NOT NULL,
+                address_id INTEGER NOT NULL,
+                email_address VARCHAR(50) NOT NULL.
+                FOREIGN KEY (address_id)
+                    REFERENCES Address (address_id));"""
+    )
 
-conn.execute('''CREATE TABLE IF NOT EXISTS ACCOUNT(ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,ACCOUNT_NUMBER CHAR(10) NOT NULL,CUSTOMER_ID INTEGER NOT NULL,CURRENT_BALANCE DECIMAL(8, 2) NOT NULL,FOREIGN KEY (CUSTOMER_ID)REFERENCES CUSTOMER (CUSTOMER_ID));''')
-print('Account table ready')
+cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS Account (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            account_number char(10) NOT NULL
+            current_balance DECIMAL (6, 2) NOT NULL,
+            FOREIGN KEY (customer_id)
+                REFERENCES Customer (customer_id));"""                                         
+    )
 
 
 cursor.execute('SELECT * FROM information_schema.tables') 
