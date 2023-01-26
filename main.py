c0b3ddac9a1e1611c1a9c901535e408fba97e08c
Albert <chenalbert60@gmail.com>
2023-01-26 16:13:49 +0000

    afdsa

--- main.py
+++ main.py
@@ -15,19 +15,14 @@ address_repository = AddressRepository()
 account_service = AccountService(account_repository, customer_repository, address_repository)
 
 @app.post('/accounts')
-async def open_account(account: Account):
+async def open_account(account):
     if account.current_balance < 25.00:
         print("Please deposit an amount greater than 25.00")
     return account_service.open_account(account)
 
 @app.get('/accounts/{account_number}')
-async def grab_account(account: Account) -> Account:
+async def grab_account(account):
     return account_service.get_account(account.account_number)
-
-# @app.get('/accounts/{account_number}/deposit/{amount}')
-# async def deposit(account_number, amount, account: Account) -> Account:
-#     account.current_balance += amount
-#     return grab_account(account)
 
 
 if __name__ == "__main__":
